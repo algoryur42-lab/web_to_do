f05967fcb83e11d0b9aac530e205e0cbfb6e3721
algoryur42-lab <algoryur42@gmail.com>
2025-11-07 21:37:49 +0700

    убрал скобку из js

--- todolistweb/static/js/app.js
+++ todolistweb/static/js/app.js
@@ -2,8 +2,6 @@ function showAddForm() {
     document.getElementById('add-form').style.display = 'block';
 }
 
-}
-
 function showCompleteForm() {
     document.getElementById('complete-form').style.display = 'block';
 }
