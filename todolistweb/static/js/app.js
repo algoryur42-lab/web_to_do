5a116e01db7c7e7ff5a8591adc1a7e67d15f0db3
algoryur42-lab <algoryur42@gmail.com>
2025-11-08 11:37:15 +0700

    Удаление ненужных функций

--- todolistweb/static/js/app.js
+++ todolistweb/static/js/app.js
@@ -1,12 +1,1 @@
-function showAddForm() {
-    document.getElementById('add-form').style.display = 'block';
-}
-
-function showCompleteForm() {
-    document.getElementById('complete-form').style.display = 'block';
-}
-
-function hideCompleteForm() {
-    document.getElementById('complete-form').style.display = 'none';
-}
 
