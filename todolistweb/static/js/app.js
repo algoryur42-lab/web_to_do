a94d22e07bf42fe84d6c5cfafcdd7049b51e2f28
algoryur42-lab <algoryur42@gmail.com>
2025-11-13 10:10:22 +0700

    Подтверждение удаления задачи

--- todolistweb/static/js/app.js
+++ todolistweb/static/js/app.js
@@ -1,1 +1,7 @@
-
+document.querySelectorAll('.delete-form').forEach(form => {
+    form.addEventListener('submit', function (e) {
+        if (!confirm('Удалить задачу?')) {
+            e.preventDefault(); // Отмена отправки
+        }
+    });
+});
